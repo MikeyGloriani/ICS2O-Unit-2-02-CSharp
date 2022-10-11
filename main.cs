061f9754bf3d4b69e365312cf5c79b365e383b7e
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-11 14:42:37 -0400

    changes

--- main.cs
+++ main.cs
@@ -5,7 +5,7 @@
 
 using System; 
 
-class Program 
+public class Program 
 {
   
     public static void Main(string[] args) 
@@ -13,6 +13,7 @@ class Program
         Console.WriteLine("The dimensions of a rectangle are 5 cm and 3 cm. What is the area and perimeter?");
         Console.WriteLine("The area is " + (5*3) + " cm²");
         Console.WriteLine("The perimeter is " + (2*(5+3)) + " cm");
+        
         Console.WriteLine("\nDone.");
     }
 }
